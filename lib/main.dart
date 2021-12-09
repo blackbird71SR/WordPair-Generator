8027237f8adcf3d07e63bd41f653f2e16ac52ed2
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2021-12-09 12:18:47 -0600

    Add WordPair project functionality

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import './random_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(primaryColor: Colors.purple[900]),
+        home: RandomWords());
+  }
+}
